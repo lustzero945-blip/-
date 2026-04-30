c9be1e23496ff61302934e7ee46f41e236f62a02
LUST DEV <lustzero945@gmail.com>
2026-04-29 20:20:27 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,66 +1,30 @@
-import json
-from config import LEVEL_MULTIPLIER
+users = {}
 
-FILE = "users.json"
-
-def load():
-    try:
-        with open(FILE, "r") as f:
-            return json.load(f)
-    except:
-        return {}
-
-def save(data):
-    with open(FILE, "w") as f:
-        json.dump(data, f, indent=2)
-
-def get_user(user_id, name="User"):
-    data = load()
-
-    if str(user_id) not in data:
-        data[str(user_id)] = {
+def get_user(user_id, name):
+    if user_id not in users:
+        users[user_id] = {
             "name": name,
             "xp": 0,
             "level": 1,
-            "quiz_score": 0,
-            "badges": []
+            "quiz_score": 0
         }
-        save(data)
-
-    return data[str(user_id)]
+    return users[user_id]
 
 def add_xp(user_id, name, amount):
-    data = load()
     user = get_user(user_id, name)
-
     user["xp"] += amount
-
-    if user["xp"] >= user["level"] * LEVEL_MULTIPLIER:
-        user["xp"] = 0
-        user["level"] += 1
-
-    data[str(user_id)] = user
-    save(data)
-    return user
+    user["level"] = user["xp"] // 50 + 1
 
 def add_quiz_score(user_id, score):
-    data = load()
-    user = data.get(str(user_id))
-    user["quiz_score"] += score
-    save(data)
+    users[user_id]["quiz_score"] += score
 
 def leaderboard():
-    data = load()
-    users = list(data.values())
-    users.sort(key=lambda x: (x["level"], x["xp"]), reverse=True)
-    return users[:10]
+    return sorted(users.values(), key=lambda x: x["level"], reverse=True)[:10]
 
 def get_rank(level):
     if level < 5:
-        return "🥉 Bronze"
+        return "Beginner"
     elif level < 10:
-        return "🥈 Silver"
-    elif level < 20:
-        return "🥇 Gold"
+        return "Intermediate"
     else:
-        return "👑 Elite"
+        return "Pro"
